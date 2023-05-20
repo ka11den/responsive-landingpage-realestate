253d6b5d3f4f40ad0d437865dc1039363da5d6ca
ka11den <ka1den@mail.ru>
2023-05-20 17:58:22 +0400

    feat: responsive small device

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// header
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close')
+
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('snow-menu');
+  })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('snow-menu');
+  })
+}
